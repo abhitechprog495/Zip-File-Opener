9aff48543d98870d1d613211c0b73f03403cb4b8
Abhishek Kumar <59432239+abhitechprog495@users.noreply.github.com>
2021-11-28 22:10:58 +0530

    Update zip_file_opener.py

--- zip_file_opener.py
+++ zip_file_opener.py
@@ -3,6 +3,8 @@ from tkinter import *
 from tkinter import filedialog
 from zipfile import ZipFile
 
+#Function to select files to unzip and also
+#select diectory to place unzip file/folder
 def OpenFile():
     file_path=filedialog.askopenfilename()
     file=open(file_path,'rb')
@@ -13,10 +15,12 @@ def OpenFile():
         zip.extractall(unzip_file)
         print("Process Completed!")
 
+#Set the window of the application
 root = Tk()
 frame = Frame(root).pack()
 root.title("Zip File Opener")
 root.geometry('400x200')
 button=Button(text="Select Zip File", command=OpenFile).pack()
 
+#Control the window
 root.mainloop()
